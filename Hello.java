2943234c19a50eaa07048c3d2047716e098af0fe
Oluwatobi Solomon <staocube@gmail.com>
2022-04-04 12:24:06 +0100

    Java test file commit for jenkins

---
+++ Hello.java
@@ -0,0 +1,7 @@
+public class Hello{
+	public static void main(String[]args){
+	//prints out an hello world message to test jenkins
+		System.out.println("hello world, Welcome to jenkins!");
+
+	}
+}
